4919093006c82dfffc32c806084104b1d7343b33
Jonathan Valdebenito Rebolledo <87279945+jonavr-dev@users.noreply.github.com>
2021-07-11 22:43:50 -0400

    Updated app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,9 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
