9764b2504435798943fb01887c9c2bc3e4f02ae7
Jonathan Valdebenito Rebolledo <87279945+jonavr-dev@users.noreply.github.com>
2021-07-15 17:25:16 -0400

    Updated environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  wsUrl: 'https://socket-server-gh-zvdkx.ondigitalocean.app',
-  restUrl: 'https://socket-server-gh-zvdkx.ondigitalocean.app'
+  wsUrl: 'https://socket-server-gh.jonavr.dev',
+  restUrl: 'https://socket-server-gh.jonavr.dev'
 };
